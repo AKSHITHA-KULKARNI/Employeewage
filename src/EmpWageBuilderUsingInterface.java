620efeb0cb432dbdc3835b983f6f55ef61f7c70a
Akshitha Kulkarni <akshitha.k03@gmail.com>
2024-12-08 19:29:01 +0530

    Added:ComputeEmployee wage

--- src/EmpWageBuilderUsingInterface.java
+++ src/EmpWageBuilderUsingInterface.java
@@ -1,39 +1,4 @@
-public class EmpWageBuilderUsingInterface implements IEmployeeWage {
-    private String companyName;
-    private int empRatePerHour;
-    private int maxWorkingDays;
-    private int maxWorkingHours;
-    private int totalWage;
+package PACKAGE_NAME;
 
-    public EmpWageBuilderUsingInterface(String companyName, int empRatePerHour, int maxWorkingDays, int maxWorkingHours) {
-        this.companyName = companyName;
-        this.empRatePerHour = empRatePerHour;
-        this.maxWorkingDays = maxWorkingDays;
-        this.maxWorkingHours = maxWorkingHours;
-        this.totalWage = 0;
-    }
-
-    @Override
-    public void computeEmployeeWage() {
-        int totalHoursWorked = 0;
-        int totalDaysWorked = 0;
-
-        while (totalDaysWorked < maxWorkingDays && totalHoursWorked < maxWorkingHours) {
-            totalDaysWorked++;
-            int dailyHoursWorked = (Math.random() > 0.5) ? 8 : 4;
-            totalHoursWorked += dailyHoursWorked;
-
-            if (totalHoursWorked > maxWorkingHours) {
-                dailyHoursWorked -= (totalHoursWorked - maxWorkingHours);
-                totalHoursWorked = maxWorkingHours;
-            }
-
-            int dailyWage = dailyHoursWorked * empRatePerHour;
-            totalWage += dailyWage;
-
-            System.out.println("Day " + totalDaysWorked + ": Worked " + dailyHoursWorked + " hours, Daily Wage = $" + dailyWage);
-        }
-
-        System.out.println("Total Wage for " + companyName + ": $" + totalWage);
-    }
+public interface EmpWageBuilderUsingInterface {
 }
